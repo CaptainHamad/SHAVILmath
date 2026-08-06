a623cdc56558540eabc4fa7e3d49b3ad445928c8
CaptainHamad <121143030+CaptainHamad@users.noreply.github.com>
2026-08-06 16:27:40 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://shnkqegaaocffjfscznu.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_VsQ1yMkQLvDQB_tj-qSQmg_G8-5crac";
 
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
