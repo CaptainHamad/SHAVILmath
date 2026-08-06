e3545253cc04d2a125a0072dd0a22a04be1f5f31
CaptainHamad <121143030+CaptainHamad@users.noreply.github.com>
2026-08-06 15:58:47 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = "WEKA_PROJECT_URL_YAKO";
+const SUPABASE_URL = "https://shnkqegaaocffjfscznu.supabase.co/rest/v1/";
 
-const SUPABASE_ANON_KEY = "WEKA_ANON_KEY_YAKO";
+const SUPABASE_ANON_KEY = "sb_publishable_VsQ1yMkQLvDQB_tj-qSQmg_G8-5crac";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
